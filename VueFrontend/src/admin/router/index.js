5e08d5c9998d146e61b8d5e5a4775a901f930d0c
minhquang111 <minhquang2532000@gmai.com>
2022-05-30 13:33:53 +0700

    up auth layouts

--- VueFrontend/src/admin/router/index.js
+++ VueFrontend/src/admin/router/index.js
@@ -1,141 +1,144 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '/',
-    name: 'Dashboard',
-    list: [{ path: '/', name: 'Dashboard' }],
+    path: "/",
+    name: "Dashboard",
+    list: [{ path: "/", name: "Dashboard" }],
+    meta: {
+      layout: "auth",
+    },
     component: () =>
-      import(/* webpackChunkName: "dashboard" */ '../views/dashboardView.vue'),
+      import(/* webpackChunkName: "dashboard" */ "../views/dashboardView.vue"),
   },
   {
-    path: '/net-ips/alert',
-    name: 'Alert',
+    path: "/net-ips/alert",
+    name: "Alert",
     list: [
       {
-        path: '/net-ips/anomalies',
-        name: 'NetIPS',
+        path: "/net-ips/anomalies",
+        name: "NetIPS",
       },
       {
-        path: '/net-ips/alert',
-        name: 'Alert',
+        path: "/net-ips/alert",
+        name: "Alert",
       },
     ],
     component: () =>
       import(
-        /* webpackChunkName: "netIPS-alert" */ '../views/netIPS/alertView.vue'
+        /* webpackChunkName: "netIPS-alert" */ "../views/netIPS/alertView.vue"
       ),
   },
   {
-    path: '/net-ips/anomalies',
-    name: 'Anomaly Events',
+    path: "/net-ips/anomalies",
+    name: "Anomaly Events",
     list: [
       {
-        path: '/net-ips/anomalies',
-        name: 'NetIPS',
+        path: "/net-ips/anomalies",
+        name: "NetIPS",
       },
     ],
     component: () =>
       import(
-        /* webpackChunkName: "netIPS-anomalies" */ '../views/netIPS/anomaliesView.vue'
+        /* webpackChunkName: "netIPS-anomalies" */ "../views/netIPS/anomaliesView.vue"
       ),
   },
   {
-    path: '/net-ips/intrusion',
-    name: 'Intrusion Monitoring',
+    path: "/net-ips/intrusion",
+    name: "Intrusion Monitoring",
     list: [
       {
-        path: '/net-ips/anomalies',
-        name: 'NetIPS',
+        path: "/net-ips/anomalies",
+        name: "NetIPS",
       },
     ],
     component: () =>
       import(
-        /* webpackChunkName: "netIPS-intrusion" */ '../views/netIPS/intrusionView.vue'
+        /* webpackChunkName: "netIPS-intrusion" */ "../views/netIPS/intrusionView.vue"
       ),
   },
   {
-    path: '/net-ips/report',
-    name: 'Report',
+    path: "/net-ips/report",
+    name: "Report",
     list: [
       {
-        path: '/net-ips/anomalies',
-        name: 'NetIPS',
+        path: "/net-ips/anomalies",
+        name: "NetIPS",
       },
     ],
     component: () =>
       import(
-        /* webpackChunkName: "netIPS-report" */ '../views/netIPS/reportView.vue'
+        /* webpackChunkName: "netIPS-report" */ "../views/netIPS/reportView.vue"
       ),
   },
   {
-    path: '/net-ips/search',
-    name: 'Search',
+    path: "/net-ips/search",
+    name: "Search",
     list: [
       {
-        path: '/net-ips/anomalies',
-        name: 'NetIPS',
+        path: "/net-ips/anomalies",
+        name: "NetIPS",
       },
     ],
     component: () =>
       import(
-        /* webpackChunkName: "netIPS-search" */ '../views/netIPS/searchView.vue'
+        /* webpackChunkName: "netIPS-search" */ "../views/netIPS/searchView.vue"
       ),
   },
   {
-    path: '/net-ips/statistics',
-    name: 'Statistics',
+    path: "/net-ips/statistics",
+    name: "Statistics",
     list: [
       {
-        path: '/net-ips/anomalies',
-        name: 'NetIPS',
+        path: "/net-ips/anomalies",
+        name: "NetIPS",
       },
     ],
     component: () =>
       import(
-        /* webpackChunkName: "netIPS-statistics" */ '../views/netIPS/statisticsView.vue'
+        /* webpackChunkName: "netIPS-statistics" */ "../views/netIPS/statisticsView.vue"
       ),
   },
   {
-    path: '/setting/role-list',
-    name: 'Role List',
+    path: "/setting/role-list",
+    name: "Role List",
     list: [
       {
-        path: '/setting/user-list',
-        name: 'Settings',
+        path: "/setting/user-list",
+        name: "Settings",
       },
     ],
     component: () =>
       import(
-        /* webpackChunkName: "settings-role-list" */ '../views/settings/roleListView.vue'
+        /* webpackChunkName: "settings-role-list" */ "../views/settings/roleListView.vue"
       ),
   },
   {
-    path: '/setting/system-setting',
-    name: 'System Settings ',
+    path: "/setting/system-setting",
+    name: "System Settings ",
     list: [
       {
-        path: '/setting/user-list',
-        name: 'Settings',
+        path: "/setting/user-list",
+        name: "Settings",
       },
     ],
     component: () =>
       import(
-        /* webpackChunkName: "settings-system" */ '../views/settings/systemSettingView.vue'
+        /* webpackChunkName: "settings-system" */ "../views/settings/systemSettingView.vue"
       ),
   },
   {
-    path: '/setting/user-list',
-    name: 'User List',
+    path: "/setting/user-list",
+    name: "User List",
     list: [
       {
-        path: '/setting/user-list',
-        name: 'Settings',
+        path: "/setting/user-list",
+        name: "Settings",
       },
     ],
     component: () =>
       import(
-        /* webpackChunkName: "settings-user-list" */ '../views/settings/userListView.vue'
+        /* webpackChunkName: "settings-user-list" */ "../views/settings/userListView.vue"
       ),
   },
 ];
--- VueFrontend/src/admin/views/settings/userListView.vue
+++ VueFrontend/src/admin/views/settings/userListView.vue
@@ -1,5 +1,48 @@
-<template>userListView</template>
+<template>
+<a-input v-model:value="value" placeholder="Basic usage" />
+ <a-button type="primary">
+    <template #icon><SearchOutlined /></template>
+    Search
+  </a-button>
+  <a-table :dataSource="dataSource" :columns="columns" />
+</template>
 <script>
-import { defineComponent } from 'vue';
-export default defineComponent({});
+  export default {
+    setup() {
+      return {
+        dataSource: [
+          {
+            key: '1',
+            name: 'Mike',
+            age: 32,
+            address: '10 Downing Street',
+          },
+          {
+            key: '2',
+            name: 'John',
+            age: 42,
+            address: '10 Downing Street',
+          },
+        ],
+
+        columns: [
+          {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+          },
+          {
+            title: 'Age',
+            dataIndex: 'age',
+            key: 'age',
+          },
+          {
+            title: 'Address',
+            dataIndex: 'address',
+            key: 'address',
+          },
+        ],
+      };
+    },
+  };
 </script>
